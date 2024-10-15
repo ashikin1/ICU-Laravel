e2f9b08a41df805262f9515d5dfb37ab74261d75
ashikin <ashikin@icu.gov.my>
2024-10-15 10:35:48 +0800

    1. home page. use config to display title, user name to display nama 2. add route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>{{ config('app.name') }}</title>
 </head>
+
 <body>
-    <h1>Hello ICU Team</h1>
+    <h1>Hello, {{ $name ?? "yorn" }}</h1>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,52 @@ Route::get('/home', function () {
 Route::get('/auth/signin', function () {
     return view('auth.signin');
 });
+
+
+Route::get('/home/{name}', function ($name) {
+    return view('home ', ['name' =>$name]);
+});
+
+//Named Route
+Route::get('/people/profile', function () {
+    return 'Profil Pengguna Sistem';
+})->name('user.profile');
+
+//Route Param
+Route::get('/user/{name}', function ($name) {
+    return 'user '.$name;
+});
+
+
+//alias of a route user.profile =/pengguna/profile
+
+//Redirect route to named route
+Route::get('/redirect-to-profile', function () {
+    return redirect()->route('user.profile');
+});
+
+//Route Group
+Route::prefix('food')->group(function () {
+
+    Route::get('/details', function () {
+        return 'Food details are following';
+    });
+
+    Route::get('/home', function () {
+        return 'Food home page';
+    });
+    
+});
+
+//Route Group
+Route::name('job')->prefix('job')->group(function () {
+
+    Route::get('home', function () {
+        return 'Job home page';
+    })->name('.home');
+
+    Route::get('details', function () {
+        return 'Job details are following';
+    })->name('.description');
+    
+});
